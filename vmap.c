029db056e2cbda3497c7e0f2aa67789905295ea7
viktor <viktor.holsvik@gmail.com>
2024-12-01 22:48:57 +0100

    Program now scans lo at hardcoded ports, prints response.

--- vmap.c
+++ vmap.c
@@ -11,18 +11,16 @@ int scan_port(int port);
 
 int main()
 {
-	char send_string[13] = "Hello World!";
-	char read_string[101];
-	puts(send_string);
 
-	int scan_socket = scan_port(9999);
-	printf("Socket file descriptor: %d\n",scan_socket);
-	write(scan_socket, &send_string, 20);
-	read(scan_socket, read_string,100);
-	read_string[100] == '\0';
-	puts(read_string);
-
-	close(scan_socket);
+	int scan_socket;
+	if ( (scan_socket = scan_port(22) ) == -1)
+	{
+		puts("No contact with port 22\n");
+	}
+	if ( (scan_socket = scan_port(21) ) == -1)
+	{
+		puts("No contact with port 21\n");
+	}
 	return 0;
 }
 
@@ -32,9 +30,8 @@ int scan_port(int port)
 	errno = 0; // Errno must be locked for multithreading 
 	if ( (new_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{
-		perror(strerror(errno));
 		close(new_socket);
-		exit(EXIT_FAILURE);
+		return -1;
 	}
 
 	struct sockaddr_in bind_address;
@@ -43,13 +40,31 @@ int scan_port(int port)
 	bind_address.sin_addr.s_addr = htonl(INADDR_LOOPBACK); // Change this to scan other devices
 	bind_address.sin_port = htons(port);
 	
+	errno = 0;
 	if (connect(new_socket, (struct sockaddr *) &bind_address, sizeof(bind_address)) < 0)
 	{
-		perror("Connect failed: ");
-		perror(strerror(errno));
+		sprintf(stderr, "Could not connect to port %d", port);
 		close(new_socket);
-		exit(EXIT_FAILURE);
+		return -1;
 	}
-	return new_socket;
+	char send_string[13] = "Hello World!";
+	char read_string[101];
+	errno = 0;
+	if (write(new_socket, &send_string, 20) == -1)
+	{
+		perror("Write: ");
+		return -1;
+	}
+	errno = 0;
+	if (read(new_socket, read_string,100) == -1)
+	{
+		perror("Read: ");
+		return -1;
+	}
+	read_string[100] == '\0';
+	puts(read_string);
+
+	close(new_socket);
+	return 1;
 }
 
